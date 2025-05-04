3b613594e69b5115296606195c7368dbf9cd951c
ItsKorin <admin@imgns.de>
2025-05-04 19:51:34 +0200

    Added some stuff

--- addons/postfx/effects/ChromaticAberrationFX.gd
+++ addons/postfx/effects/ChromaticAberrationFX.gd
@@ -1,6 +1,6 @@
 @tool
 extends FXBase
-class_name ChromaticAbberationFX
+class_name ChromaticAberrationFX
 
 @export var strength : float = 2.0:
 	set(value):
@@ -9,7 +9,7 @@ class_name ChromaticAbberationFX
 
 
 func _get_shader_code() -> String:
-	return load("res://addons/postfx/shaders/chromatic_abberation.gdshader").code
+	return load("res://addons/postfx/shaders/chromatic_aberration.gdshader").code
 
 func _update_shader() -> void:
 	properties["offset"] = strength
