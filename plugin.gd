bc5e7e80ca0b7b7ef34fadb8c25a60bb2922b59c
@sanyabeast <a.gvrnsk@gmail.com>
2025-09-23 19:44:19 +0300

    Add postfx_node icon and update plugin.gd
    Added new SVG icon and its import file for postfx_node in the assets directory. Updated plugin.gd to reference or utilize the new icon.

--- plugin.gd
+++ plugin.gd
@@ -6,7 +6,8 @@ func _enter_tree() -> void:
 	add_custom_type(
 		"PostFX", 
 		"CanvasLayer", 
-		preload("res://addons/postfx/nodes/PostFX.gd")
+		preload("res://addons/postfx/nodes/PostFX.gd"),
+		preload("res://addons/postfx/assets/postfx_node.svg")
 	)
 
 func _exit_tree() -> void:
